de9a56182cc0a9253fbc77b8a548a7c97bff7e26
choukr11 <hrbchoukri@gmail.com>
2024-01-30 09:37:34 +0100

    Initial Commit for this project

---
+++ src/Vehicle.java
@@ -0,0 +1,30 @@
+public class Vehicle {
+    private String make;
+    private String model;
+    private double rentalPrice;
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setRentalPrice(double rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getRentalPrice() {
+        return rentalPrice;
+    }
+
+
+}
